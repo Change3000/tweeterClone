2e50c1f194707f55f2ad36d811e1137008eba1a2
Change3000 <87793316+Change3000@users.noreply.github.com>
2024-06-10 05:44:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
-import React, { useState } from 'react';
-import './styles/App.css';
+import React from 'react';
+import Register from './views/Register';
+import Home from './views/Home';
+import Profile from './views/Profile';
+import Login from './views/Login';
 import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import RightSidebar from './components/RightSidebar';
-import Feed from './components/Feed';
+import TweetCard from './components/Tweet';
+import './assets/index.css';
+import './assets/tailwind.css';
 
 function App() {
-  const [tweets, setTweets] = useState([
-    { id: 1, content: "Hello World!", comments: [], likes: 0 },
-    { id: 2, content: "My first tweet!", comments: [], likes: 0 }
-  ]);
-
-  const addTweet = (tweetContent) => {
-    setTweets([...tweets, { id: tweets.length + 1, content: tweetContent, comments: [], likes: 0 }]);
-  };
-
-  const addComment = (tweetId, comment) => {
-    setTweets(tweets.map(tweet => tweet.id === tweetId ? { ...tweet, comments: [...tweet.comments, comment] } : tweet));
-  };
-
-  const likeTweet = (tweetId) => {
-    setTweets(tweets.map(tweet => tweet.id === tweetId ? { ...tweet, likes: tweet.likes + 1 } : tweet));
-  };
-
   return (
-    <div className="app">
-      <Navbar />
-      <div className="container">
-        <Sidebar />
-        <Feed tweets={tweets} addTweet={addTweet} addComment={addComment} likeTweet={likeTweet} />
-        <RightSidebar />
-      </div>
+    <div className='flex w-full' >
+      <Register/>
+      <Login/>
+      <Navbar/>
+      <TweetCard/>
+      <Profile/>
+      <Home />
     </div>
   );
 }
