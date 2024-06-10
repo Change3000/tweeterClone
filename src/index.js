ad3038ba9738c7919d6bb55168344cd76ead7bb5
Change3000 <87793316+Change3000@users.noreply.github.com>
2024-06-10 05:46:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import './index.css'; 
+import App from './App'; 
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const HtmlHead = () => (
+  <>
+    <meta charSet="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/src/favicon.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Twitter</title>
+    <link href="http://fonts.cdnfonts.com/css/roboto" rel="stylesheet" />
+  </>
 );
 
+const Root = () => (
+  <div className="bg-black text-white">
+    <App />
+  </div>
+);
 
+const rootElement = document.getElementById('root');
 
+ReactDOM.render(
+  <React.StrictMode>
+    <HtmlHead />
+    <Root />
+  </React.StrictMode>,
+  rootElement
+);
