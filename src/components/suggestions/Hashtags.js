601d9632c0c2dd65f6e6b7cd5cc7466e8ac15342
Change3000 <87793316+Change3000@users.noreply.github.com>
2024-06-10 21:03:19 +0530

    Update Hashtags.js

--- src/components/suggestions/Hashtags.js
+++ src/components/suggestions/Hashtags.js
@@ -1,41 +1,37 @@
 import { useState } from "react";
 import More from '../../assets/more.svg';
-import '../../styles/Hashtags.css'; 
 
 function App() {
-
     const [tags, setTags] = useState([
         "#shutdowndevelopers",
         "Buzz",
         "A big event",
         "#Lorem",
         "#Ipsum",
-        "#opendevelopers",
-        "DEVELOPER SALARIES"
+        "#opentothepublic",
+        "SOFTWARE DEVELOPER SALARIES"
     ]);
 
     return (
-        <div id='hashtags' className='hashtags-container'>
-            <span className='title'>Topics you may be interested in</span>
-            <div className='spacer'></div>
-            {
-                tags.map((item, index) => {
-                    return (
-                        <div key={index} className='tag-item'>
-                            <div className='tag-content'>
-                                <div className='tag-header'>
-                                    <span className='tag-category'>Politics - Trending</span>
-                                    <img src={More} width="20" alt="" />
-                                </div>
-                                <span className="tag-name">{item}</span>
-                            </div>
-                            <span className='tag-tweets'> {Math.floor(Math.random() * (10000 - 1500) + 1500)} Tweets</span>
+        <div id='hashtags' className='w-full bg-[#16181C] rounded-2xl pt-3'>
+            <span className='font-bold text-xl px-4 pb-2'>Trending topics that may interest you</span>
+            <div className='h-[12px]'></div>
+
+            {tags.map((item, index) => (
+                <div key={index} className='flex items-start flex-col px-4 py-2 w-full cursor-pointer transition-all hover:bg-[#1D1F23]'>
+                    <div className='flex items-start flex-col w-full'>
+                        <div className='flex justify-between items-center w-full'>
+                            <span className='text-sm text-[#54595D]'>Politics - Current Affairs</span>
+                            <img src={More} width="20" alt="" />
                         </div>
-                    );
-                })
-            }
-            <div className='show-more'>
-                <span className='show-more-text'>Show more</span>
+                        <span className="font-semibold">{item}</span>
+                    </div>
+                    <span className='text-sm text-[#54595D]'>{Math.floor(Math.random() * (10000 - 1500) + 1500)} Tweets</span>
+                </div>
+            ))}
+
+            <div className='flex items-start flex-col px-4 py-3 mt-2 w-full cursor-pointer transition-all hover:bg-[#1D1F23] rounded-b-2xl'>
+                <span className='text-[#1d9bf0]'>Show more</span>
             </div>
         </div>
     );
