eb4de2bcc1961ef8bbcd2fda14212fdf297e28df
Change3000 <87793316+Change3000@users.noreply.github.com>
2024-06-10 20:45:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import Register from './views/Register';
-import Home from './views/Home';
-import Profile from './views/Profile';
-import Login from './views/Login';
-import Navbar from './components/Navbar';
-import TweetCard from './components/Tweet';
+import Login from './components/Login.js'
+import Signup from './components/Signup.js'
+import Home from './views/Home.js'
+import Navbar from './components/Navbar.js'
 import './assets/index.css';
 import './assets/tailwind.css';
 
+// call when backend set
+// <Login/>
+// <Signup/>
 function App() {
   return (
     <div className='flex w-full' >
-      <Register/>
-      <Login/>
       <Navbar/>
-      <TweetCard/>
-      <Profile/>
-      <Home />
+      <Home/>
+      
     </div>
   );
 }
